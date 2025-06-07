8de830dfb391041ee7beb00d714cb96fd799ace6
angeloc912l <angelotrotta1@gmail.comm>
2025-06-07 18:15:47 +0200

    Aggiunto file badwords-it.js con parole personalizzate

---
+++ badwords-it.js
@@ -0,0 +1,12 @@
+// badwords-it.js
+module.exports = [
+  "vaffanculo",
+  "stronzo",
+  "coglione",
+  "figlio di puttana",
+  "cazzo",
+  "merda",
+  "frocio",
+  "bastardo",
+  // aggiungi tutte quelle che vuoi
+];
